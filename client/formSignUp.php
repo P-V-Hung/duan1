c2e9aa3396468cb7dfe0d202dac86f2cde7ec7d1
P-V-Hung <azhung08102004@gmail.com>
2023-11-11 10:15:01 +0700

    validate form

---
+++ client/formSignUp.php
@@ -0,0 +1,39 @@
+<div class="signUp">
+    <h2>Đăng ký</h2>
+    <form action="">
+        <div><input type="text" placeholder="Tên đăng nhập" id="user" class="mb20">
+        <p class="errUser">Chỉ chữ và số dài 8 kí tự</p>    
+    </div>
+        <div><input type="text" placeholder="Email" id="email" class="mb20">
+    </div>
+        <div><input type="password" id="pass" placeholder="Mật khẩu"><div class="icon"><i class="fa-solid fa-eye"></i></div></div>
+        <div><input type="password" id="repass" placeholder="Nhập lại mật khẩu"><div class="icon"><i class="fa-solid fa-eye"></i></div></div>
+        <input type="submit" name="btn_signUp" value="Đăng ký">
+    </form>
+    <div class="more">
+        Bạn đã có có tài khoản? 
+        <a href="#">Đăng nhập</a>
+    </div>
+</div>
+<script>
+    function validate(input,error){
+        input.addEventListener('input',function(){
+        let val = input.value;
+        if(/^[a-zA-Z0-9]{8,}$/.test(val)){
+            error.style.color = "green";
+
+        }else{
+            error.style.color = "red";
+        }
+        });
+    }
+
+    let username = document.querySelector("#user");
+    let errUser = document.querySelector(".errUser");
+    let pass = document.querySelector("#pass");
+
+    validate(username,errUser);
+
+    
+    
+</script>
--- index.php
+++ index.php
@@ -5,9 +5,9 @@
     // require "./client/product.php";
     // require "./client/userInfo.php";
     // 
-    require "./client/thanhtoan.php";
+    // require "./client/thanhtoan.php";
     // require "./client/cart.php";
-    // require "./client/formSingin.php";
-    // require "./client/formSingup.php";
+    // require "./client/formSignin.php";
+    require "./client/formSignup.php";
     require "./client/footer.php";
 ?>
