ad6c41c4b0fafce31adcd8f02c6d0e29edd73fec
P-V-Hung <azhung08102004@gmail.com>
2023-11-24 22:53:10 +0700

    update

--- controllers/client/account/login.php
+++ controllers/client/account/login.php
@@ -4,7 +4,7 @@ if(isset($_POST['btn_signIn'])&&($_POST['btn_signIn'])){
     $pass=$_POST['pass'];
      
     $user=AccountFind("u_username like '%$tendn%' and u_password like '%$pass%'");
-    if($user['u_status'] == 1){
+    if($user['u_status'] == 2){
         logError("Tài khoản này đã bị khóa");
     }else{
         $_SESSION['user'] = $user;
