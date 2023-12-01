ce42a67409e125d53afcd0e7f1d1794970dd1488
Quang Hien <dqhien2004@gmail.com>
2023-12-01 21:26:17 +0700

    chart

--- controllers/admin/chart/chart.php
+++ controllers/admin/chart/chart.php
@@ -1,17 +1,3 @@
-<div class="right-sitebar container-xxl">
-    <h2 class="py-4 title-admin">Thống kê</h2>
-    <div class="container-xl p-2">
-        <div class="row">
-            <div class="col"><?php require_once ("chartCatCat.php"); ?></div>
-            <div class="col"><?php require_once ("chartCatPro.php"); ?></div>
-        </div>
-        <div class="row">
-            <div class="col"><?php require_once ("chartBill.php"); ?></div>
-            <div class="col"><?php require_once ("chartAccount.php"); ?></div>
-        </div>
-        <div class="row mb-5">
-            <?php require_once ("chartComment.php"); ?>
-        </div>
-    </div>
-
-</div>
+<?php 
+    require_once $views."chart/chart.php";
+?>
