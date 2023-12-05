ca7a48cb7875cdb5dac2c7e70921d0789b3add96
Quang Hien <dqhien2004@gmail.com>
2023-12-05 11:02:59 +0700

    Thêm thông báo admin

--- controllers/admin/voucher/list.php
+++ controllers/admin/voucher/list.php
@@ -1,15 +1,22 @@
 <?php 
     $listVouchers = VoucherAll();
+    $check = false;
     if(isset($_POST['btn-deletes-v'])){
         $btn = array_pop($_POST);
         foreach($_POST as $id){
             VoucherDelete($id);
-            reUrl('voucher/list');
+            $check = true;
+            // reUrl('voucher/list');
         }
+        if($check) $_SESSION["delVous"] = true;
     } 
     if(isset($_SESSION['delVou'])){
         logSuccess('Xóa mã giảm giá thành công');
         unset($_SESSION['delVou']);
+    }
+    if(isset($_SESSION['delVous'])){
+        logSuccess('Xóa nhiều mã giảm giá thành công');
+        unset($_SESSION['delVous']);
     }
     require $views."voucher/list.php";
 ?>
